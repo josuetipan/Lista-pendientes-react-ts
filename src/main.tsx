cd25583d2392c3fa7c2ddb62aee9c08a6d3ff248
santiagoMosquera99 <krakedev3@gmail.com>
2024-09-15 21:17:12 -0500

    Proyecto final de lista de tareas con react y typescript

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import "./styles.css"
+import { TodoApp } from './componentes/TodoApp'
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <TodoApp></TodoApp>
+  </StrictMode>,
+)
